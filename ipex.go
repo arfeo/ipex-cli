80d7b83e93f1609ae08cc8ff6d1a8075eb44c1df
Leonid Belikov <mail@arfeo.net>
2019-01-23 21:57:18 +0300

    refactor

--- ipex.go
+++ ipex.go
@@ -1,89 +1,94 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io/ioutil"
-	"strings"
-	"github.com/antchfx/xmlquery"
-	"net/url"
-	"log"
-	"time"
-	"runtime"
-	"os/user"
-	"io"
 	"bufio"
+	"fmt"
+	"github.com/antchfx/xmlquery"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"os/user"
+	"runtime"
 	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-
 	var (
-		iml_file string
-		import_dir string
-		files_copied int = 0
-		error_count int = 0
-		curr_playlist int
-		user_os string = runtime.GOOS
-		xml_found bool
+		imlFile       string
+		importDir     string
+		filesCopied   = 0
+		errorCount    = 0
+		currPlaylist  int
+		userOs        = runtime.GOOS
+		isXmlFound    bool
 	)
 
 	// Only run on macOS or Windows
-	if user_os == "darwin" || user_os == "windows" {
+	if userOs == "darwin" || userOs == "windows" {
 
 		// Initialize new reader globally
 		in := bufio.NewReader(os.Stdin)
 
 		// Setting up the default path to XML file depending on username and OS
-		user, _ := user.Current()
-		switch user_os {
-		case "darwin": iml_file = "/Users/" + user.Username + "/Music/iTunes/iTunes Music Library.xml"
-		case "windows": iml_file = "C:\\Users\\" + strings.Split(user.Username, "\\")[1] + "\\Music\\iTunes\\iTunes Music Library.xml"
+		systemUser, _ := user.Current()
+		switch userOs {
+		case "darwin": imlFile = "/Users/" + systemUser.Username + "/Music/iTunes/iTunes Music Library.xml"
+		case "windows": imlFile = "C:\\Users\\" + strings.Split(systemUser.Username, "\\")[1] + "\\Music\\iTunes\\iTunes Music Library.xml"
 		}
 
-	GET_XML:
+	GetXml:
 
 		// Searching for XML
-		fmt.Println("Searching for iTunes Media Library XML in:", iml_file, "...", func () string {
-			if FileExists(iml_file) {
-				xml_found = true
+		fmt.Println("Searching for iTunes Media Library XML in:", imlFile, "...", func () string {
+			if FileExists(imlFile) {
+				isXmlFound = true
+
 				return "\033[32mfound.\033[0m"
 			} else {
-				xml_found = false
+				isXmlFound = false
+
 				return "\033[31mnot found.\033[0m"
 			}
 		}())
 
 		// If iTunes Media Library XML file found -- read and parse it
-		if xml_found {
-
+		if isXmlFound {
 			playlists := make(map[int]string)
 
 			// Read XML file
-			file, err := ioutil.ReadFile(iml_file)
+			file, err := ioutil.ReadFile(imlFile)
+
 			if err != nil {
 				log.Fatal(err)
 			}
+
 			str := string(file)
 
-		CREATE_IMPORT_DIR:
+		CreateImportDir:
 
 			// Get the name of import directory from command line
 			fmt.Println("Enter the name of import directory:")
-			input_id, _ := in.ReadString('\n')
-			import_dir = strings.TrimRight(input_id, "\r\n")
+			inputId, _ := in.ReadString('\n')
+			importDir = strings.TrimRight(inputId, "\r\n")
 
 			// Create import directory
-			if !FileExists(import_dir) {
-				if err := os.Mkdir(import_dir, 0777); err != nil {
+			if !FileExists(importDir) {
+				if err := os.Mkdir(importDir, 0777); err != nil {
 					fmt.Println("\033[31mCannot create directory.\033[0m")
-					goto CREATE_IMPORT_DIR
+
+					goto CreateImportDir
 				}
 			}
 
 			// Parse XML data...
 			var reader= strings.NewReader(str)
+
 			doc, err := xmlquery.Parse(reader)
+
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -100,23 +105,27 @@ func main() {
 				log.Fatal("\033[33mNo playlists found.\033[0m")
 			}
 
-		GET_PLAYLIST:
+		GetPlaylist:
 
 			// Get a playlist number from command line
 			fmt.Println("Enter a playlist number:")
-			inp_cp, _ := in.ReadString('\n')
-			curr_playlist, _ = strconv.Atoi(strings.TrimRight(inp_cp, "\r\n"))
-			if _, ok := playlists[curr_playlist - 1]; ok {
-				fmt.Println("Playlist selected:", playlists[curr_playlist-1])
+			inputCurrPlaylist, _ := in.ReadString('\n')
+			currPlaylist, _ = strconv.Atoi(strings.TrimRight(inputCurrPlaylist, "\r\n"))
+
+			if _, ok := playlists[currPlaylist- 1]; ok {
+				fmt.Println("Playlist selected:", playlists[currPlaylist-1])
 			} else {
 				fmt.Println("\033[31mWrong playlist number.\033[0m")
-				goto GET_PLAYLIST
+
+				goto GetPlaylist
 			}
 
 			// ... extract tracks
-			nodeTracks := xmlquery.Find(doc, ".//dict/key[.='Playlists']/following-sibling::array//string[.='"+playlists[curr_playlist-1]+"']/following-sibling::array/dict/integer/text()")
+			nodeTracks := xmlquery.Find(doc, ".//dict/key[.='Playlists']/following-sibling::array//string[.='"+playlists[currPlaylist-1]+"']/following-sibling::array/dict/integer/text()")
+
 			if len(nodeTracks) > 0 {
 				fmt.Println("Extracting playlists...")
+
 				for _, n := range nodeTracks {
 					p := n.InnerText()
 
@@ -124,11 +133,12 @@ func main() {
 					trackPath, _ := url.PathUnescape(xmlquery.FindOne(doc, ".//dict/key[.='Tracks']/following-sibling::dict/key[.='"+p+"']/following-sibling::dict/key[.='Location']/following-sibling::string/text()").InnerText())
 
 					b := strings.Split(trackPath, "/")
-					dir := import_dir
+					dir := importDir
 
 					// Create a directory inside the import one for each file
 					for i := 3; i >= 2; i-- {
 						dir += "/" + b[len(b)-i]
+
 						if !FileExists(dir) {
 							if err := os.Mkdir(dir, 0777); err != nil {
 								log.Println(err)
@@ -140,11 +150,11 @@ func main() {
 					t := strings.Replace(trackPath, "file:///", "/", -1)
 
 					// Copy files to the import directory
-					if CopyFile(t, import_dir+"/"+b[len(b)-3]+"/"+b[len(b)-2]+"/"+b[len(b)-1]) {
+					if CopyFile(t, importDir+"/"+b[len(b)-3]+"/"+b[len(b)-2]+"/"+b[len(b)-1]) {
 						fmt.Println("\033[32mCopied successfully:\033[0m", t)
-						files_copied++
+						filesCopied++
 					} else {
-						error_count++
+						errorCount++
 					}
 				}
 
@@ -152,23 +162,22 @@ func main() {
 				time.Sleep(time.Second)
 
 				// ... then exit normally
-				defer fmt.Println("Done.", len(nodeTracks), "tracks in '"+playlists[curr_playlist-1]+"' playlist.", files_copied, "files copied.", error_count, "errors.")
+				defer fmt.Println("Done.", len(nodeTracks), "tracks in '"+playlists[currPlaylist-1]+"' playlist.", filesCopied, "files copied.", errorCount, "errors.")
 			} else {
 				fmt.Println("\033[33mPlaylist is empty.\033[0m")
-				goto GET_PLAYLIST
-			}
 
+				goto GetPlaylist
+			}
 		} else {
 			fmt.Println("Enter the path to iTunes Media Library XML file:")
-			input_xml, _ := in.ReadString('\n')
-			iml_file = strings.TrimRight(input_xml, "\r\n")
-			goto GET_XML
-		}
+			inputImlFile, _ := in.ReadString('\n')
+			imlFile = strings.TrimRight(inputImlFile, "\r\n")
 
+			goto GetXml
+		}
 	} else {
 		log.Fatal("\033[31mUnsupported operating system.\033[0m")
 	}
-
 }
 
 func FileExists(name string) bool {
@@ -177,28 +186,50 @@ func FileExists(name string) bool {
 			return false
 		}
 	}
+
 	return true
 }
 
 func CopyFile(src string, dest string) bool {
 	from, err := os.Open(src)
+
 	if err != nil {
 		log.Println(err)
+
 		return false
 	}
-	defer from.Close()
+
+	defer func() {
+		err := from.Close()
+
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	to, err := os.OpenFile(dest, os.O_RDWR | os.O_CREATE, 0777)
+
 	if err != nil {
 		log.Println(err)
+
 		return false
 	}
-	defer to.Close()
+
+	defer func() {
+		err := to.Close()
+
+		if err != nil {
+			log.Println(err)
+		}
+	}()
 
 	_, err = io.Copy(to, from)
+
 	if err != nil {
 		log.Println(err)
+
 		return false
 	}
+
 	return true
 }
